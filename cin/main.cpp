3a68971508a07033ceeb85ab0f0ac4be41b81622
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-03-21 09:26:24 -0500

    ejemplo cin

---
+++ cin/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout<<"ingresa el primer valor :"<<endl;
+    cin>>x;
+    cout<<"ingresa el segundo valor :"<<endl;
+    cin>>y;
+    float div=x/y;
+    cout<<"la division es:"<<div<<endl;
+    return 0;
+}
